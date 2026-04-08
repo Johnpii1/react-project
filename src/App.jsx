800de6d0c41beeb8fac2cb34282369f555243da5
Johnpii1 <okolichimobi08@gmail.com>
2026-04-08 17:37:49 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./conponent/Navbar"
+import Hero from "./conponent/Hero"
+
+
+function App() {
+  return (
+    <div className="bg-[#3B5D50] min-h-screen w-full">
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
