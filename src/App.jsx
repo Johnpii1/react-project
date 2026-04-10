8e25c928903b23761c5632b686527771e1076a32
Johnpii1 <okolichimobi08@gmail.com>
2026-04-10 07:07:07 +0100

    updated the future page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Navbar from "./conponent/Navbar"
 import Hero from "./conponent/Hero"
+import Futures from "./conponent/Futures"
 
 
 function App() {
-  return (
-    <div className="bg-[#3B5D50] min-h-screen w-full">
+  return ( 
+  <div>
+    <div className="bg-[#3B5D50]  w-full">
       <Navbar />
       <Hero />
+    </div>
+
+   <div>
+    <Futures />
+   </div>
+
     </div>
   )
 }
---
+++ src/conponent/Futures.jsx
@@ -0,0 +1,41 @@
+import chaior from "../assets/chaior.png"
+
+
+function Futures() {
+    return (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 m-4 mt-10">
+<div className="flex flex-col gap-4 md:gap-8">
+<h3 className="font-semibold text-lg">Crafted with excellent material.</h3>
+
+<p className="text-sm text-gray-500">Quisque mi urna, consequat quam in, varius consequat lacus. In et nisi lobortis felis ornare semper pellentesque.</p>
+
+ <button className='bg-black  rounded-full text-sm text-white px-6 py-3 font-semibold w-fit'>
+Explore
+    </button>
+
+</div>
+
+
+<div className="relative bg-gray-300 rounded-lg h-auto w-[40vw] md:w-[15vw] py-8 flex justify-center items-center">
+
+  <img
+    className="absolute -top-9 left-1/2 -translate-x-1/2 w-20 object-contain"
+    src={chaior}
+    alt="chair"
+  />
+
+<div className="flex flex-col items-center gap-2 mt-8">
+<h4>Nordic Chair</h4>
+<p>$50.00</p>
+</div>
+
+</div>
+
+
+</div>
+
+    )
+}
+
+
+export default Futures;
